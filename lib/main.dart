c4789c0ccf88772c7603828de4562f0837d6b871
Mateo Llerena <mateollerena40@gmail.com>
2026-02-25 09:58:44 -0500

    En moviles manteniendo la posicion vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kiosco_au/config/config.dart';
 import 'package:kiosco_au/presentation/providers/providers.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
-void main(){
   runApp(
-    ProviderScope(child: App())
+    const ProviderScope(
+      child: App(),
+    ),
   );
 }
+
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colorTheme = ref.watch(selectColorThemeProvider);
     final isDark = ref.watch(isDarkThemeProvider);
+
     return MaterialApp.router(
       theme: AppTheme(
         isDark: isDark,
-        selectedColor: colorTheme
+        selectedColor: colorTheme,
       ).getTheme(),
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      
     );
   }
 }
