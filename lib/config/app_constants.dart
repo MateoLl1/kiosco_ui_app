9c04d94bf941a17cdbec35446ef58a48715b9f4f
Mateo Llerena <108076739+MateoLl1@users.noreply.github.com>
2026-07-16 15:46:14 -0500

    Reduciendo tiempo de consulta a la base de datos

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -9,7 +9,7 @@ class AppDurations {
   static const Duration ttsReceiveTimeout = Duration(seconds: 30);
 
   static const Duration overlayDisplay = Duration(seconds: 8);
-  static const Duration screenRefresh = Duration(seconds: 10);
+  static const Duration screenRefresh = Duration(seconds: 5);
   static const Duration welcomeRedirect = Duration(seconds: 3);
 
   static const Duration kioskIdle = Duration(seconds: 10);
